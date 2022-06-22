c509b7d82b022531b8fe6dfd9834b1935260f643
Rajat <2020kucp1064@iiitkota.ac.in>
2022-06-22 20:44:10 +0530

    all files added

---
+++ src/components/cards/Card_details.jsx
@@ -0,0 +1,29 @@
+import image5 from '../../assets/icons/carbon_notebook.svg'
+import image6 from '../../assets/icons/Vector.svg'
+import image7 from '../../assets/icons/Robot.svg'
+import image8 from '../../assets/icons/IdentificationCard.svg'
+
+const card_details=[
+    {
+      icon:image5,
+      heading:'Prove your skills',
+      data:'Gain substantial experience by solving real-world problems and pit against others to come up with innovative solutions.'
+    },
+    {
+      icon:image6,
+      heading:'Learn from community',
+      data:'One can look and analyze the solutions submitted by the other Data Scientists in the community and learn from them.'
+    },
+    {
+      icon:image7,
+      heading:'Challenge yourself',
+      data: 'There is nothing for you to lose by participating in a challenge. You can fail safe, learn out of the entire experience and bounce back harder.'
+    },
+    {
+      icon:image8,
+      heading:'Earn recognition',
+      data:'You will stand out from the crowd if you do well in Al challenges, it not only helps you shine in the community but also earns rewards.'
+    }
+  ]
+
+  export default card_details;
