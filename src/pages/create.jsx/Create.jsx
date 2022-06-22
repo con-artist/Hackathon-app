5c65fe0323ed8795f46131cb7251302f90d567e0
RAJAT SINGH <82517773+con-artist@users.noreply.github.com>
2022-06-22 21:05:24 +0530

    Update Create.jsx
    error fixed

--- src/pages/create.jsx/Create.jsx
+++ src/pages/create.jsx/Create.jsx
@@ -39,7 +39,7 @@ const Create = () => {
       
     };
 
-    if (data.name != "" && data.desc != "" && data.src != "") {
+    if (data.name != "" && data.desc != "" && data.image != "") {
       Card_data.push(data);
       // console.log(Card_data);
       navigate('/');
